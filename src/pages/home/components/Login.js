9f879a341a4143ced47054cbf2b5f3820e070126
anjali-ojha17 <anjaliojhazenith@gmail.com>
2024-08-31 19:57:10 +0530

    colors

--- src/pages/home/components/Login.js
+++ src/pages/home/components/Login.js
@@ -1,20 +1,38 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faCircleQuestion, faMessage } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faCircleQuestion,
+  faMessage,
+} from "@fortawesome/free-regular-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Login = ({ pare, email, password, submit}) => {
+const Login = ({ pare, email, password, submit }) => {
+  const notify = () => {
+    toast.success("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
   return (
     <div className="box" id="loginContainer">
       <div className="myDiv" id="loginForm">
         <div className="tt">
           <button className="divClose" onClick={() => pare(4)}>
-          <FontAwesomeIcon icon={faXmark} />
+            <FontAwesomeIcon icon={faXmark} />
           </button>
         </div>
         <form id="form2" className="forms" method="post" onSubmit={submit}>
           <div className="heading">
-            <span>Login to skillWeave</span>
+            <span className="basic">Login to skillWeave</span>
           </div>
           <div className="enroll">
             <h3>Username</h3>
@@ -39,13 +57,20 @@ const Login = ({ pare, email, password, submit}) => {
             />
           </div>
           <div className="confirm">
-            <button className="submit">Login</button>
+            <button onclick={notify} className="submit">
+              Login
+            </button>
+            <ToastContainer />
           </div>
         </form>
         <div className="liner">
           <h5>Want to register?</h5>
           <div className="spacer"></div>
-          <button onClick={() => pare(6)} className="redirect" onclick="changeZIndex(6)">
+          <button
+            onClick={() => pare(6)}
+            className="redirect"
+            onclick="changeZIndex(6)"
+          >
             <u>Login</u>
           </button>
         </div>
--- src/pages/home/components/SignUp.js
+++ src/pages/home/components/SignUp.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faCircleQuestion, faMessage } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faCircleQuestion,
+  faMessage,
+} from "@fortawesome/free-regular-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-const SignUp = ({ pare, username, email, password, submit}) => {
+const SignUp = ({ pare, username, email, password, submit }) => {
   return (
     <div className="box" id="signContainer">
-      <div className="myDiv" id="signUpForm" >
+      <div className="myDiv" id="signUpForm">
         <div className="tt">
           <button className="divClose" onClick={() => pare(3)}>
             <FontAwesomeIcon icon={faXmark} />
@@ -13,7 +17,7 @@ const SignUp = ({ pare, username, email, password, submit}) => {
         </div>
         <form id="form1" className="forms" method="post" onSubmit={submit}>
           <div className="heading">
-            <span>Sign in to skillWeave</span>
+            <span className="basic">Sign in to skillWeave</span>
           </div>
           <div className="enroll columner">
             <h3>Username</h3>
@@ -29,12 +33,22 @@ const SignUp = ({ pare, username, email, password, submit}) => {
           <div className="enroll columner">
             <h3>Email Address</h3>
             <div className="spacer"></div>
-            <input type="text" name="batch" placeholder="something@gmail.com" onChange={(e) => email(e.target.value)} />
+            <input
+              type="text"
+              name="batch"
+              placeholder="something@gmail.com"
+              onChange={(e) => email(e.target.value)}
+            />
           </div>
           <div className="password columner">
             <h3>Password</h3>
             <div className="spacer"></div>
-            <input type="password" name="password" placeholder="Password" onChange={(e) => password(e.target.value)}/>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(e) => password(e.target.value)}
+            />
           </div>
           <div className="confirm">
             <button className="submit" type="submit">

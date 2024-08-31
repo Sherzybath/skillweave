fcd777b30ede58895ba1b3c89b41d7d5d52b7d9a
Abhinav Sharma <abhinavsh2304@gmail.com>
2024-08-31 13:23:27 +0530

    added card hover animation

--- src/pages/home/components/Home.js
+++ src/pages/home/components/Home.js
@@ -1,29 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // Import the image from your assets folder
-import bigPic from '../Assets/biggpic.png';
+import bigPic from "../Assets/biggpic.png";
 
 const Home = () => {
   return (
-    <div className='Home'>
-      <div className='HomeLeft Column'>
+    <div className="Home">
+      <div className="HomeLeft Column">
         <span>End your work</span>
         <span>Woes today!</span>
-        <span>We provide business blah blah blah blah blah blah blah blah blah blah blah blah</span>
-        <Link to="/dashboard"><button>Find Work</button></Link>
+        <span>
+          Discover endless remote freelance opportunities tailored to your
+          skills. Connect with top clients, showcase your talent, and elevate
+          your career from anywhere in the world.
+        </span>
+        <Link to="/dashboard">
+          <button>Find Work</button>
+        </Link>
       </div>
-      <div className='HomeMiddle Column' >
+      <div className="HomeMiddle Column">
         {/* Display the imported image */}
         <img src={bigPic} alt="Random Pic" />
       </div>
-      <div className='HomeRight Column'>
+      <div className="HomeRight Column">
         <span>Find great work</span>
         <span>Opportunities</span>
-        <span>Search for great work blah blah blah blah blah blah blah blah blah blah blah blah</span>
-        <Link to="/dashboardF"><button>Find Talent</button></Link>
+        <span>
+          Find skilled freelancers for your projects with ease. Connect with top
+          talent, streamline your hiring process, and bring your ideas to life
+          with expert support.
+        </span>
+        <Link to="/dashboardF">
+          <button>Find Talent</button>
+        </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
